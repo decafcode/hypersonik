74707a1de6bd31ec52a2a67a2c4e8385a9f53856
Decaf Code <git@decafcode.org>
2018-10-24 18:19:02 -0400

    Add precompiled header

---
+++ precompiled.h
@@ -0,0 +1,14 @@
+#include <windows.h>
+
+#include <audioclient.h>
+#include <dsound.h>
+#include <mmdeviceapi.h>
+#include <mmreg.h>
+
+#include <assert.h>
+#include <process.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
